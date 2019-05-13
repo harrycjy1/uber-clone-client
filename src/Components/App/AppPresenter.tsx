ca4406d4611a338d9fbf0f49bbd73e4533be7b17
Choddol <choddol@jojin-yeongs-MacBook-Pro.local>
2019-05-13 23:01:55 +0900

    done #2.11

--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import AddPlace from "../../Routes/AddPlace";
+import EditAccount from "../../Routes/EditAccount";
+import Home from "../../Routes/Home";
+import OutHome from "../../Routes/OutHome";
+import PhoneLogin from "../../Routes/PhoneLogin";
+import Places from "../../Routes/Places";
+import Ride from "../../Routes/Ride";
+import Settings from "../../Routes/Settings";
+import VerifyPhone from "../../Routes/VerifyPhone";
+import SocialLogin from "../../Routes/SocialLogin";
+import FindAddress from "../../Routes/FindAddress";
 
 interface IProps {
   isLoggedIn: boolean;
 }
 
-const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => {
-  return isLoggedIn ? <span>you are in</span> : <span>you are out</span>;
-};
+const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => (
+  <BrowserRouter>
+    {isLoggedIn ? <LoggedOutRoutes /> : <LoggedInRoutes />};
+  </BrowserRouter>
+);
+
+const LoggedOutRoutes: React.SFC = () => (
+  <Switch>
+    <Route path={"/"} exact={true} component={OutHome} />
+    <Route path={"/phone-login"} component={PhoneLogin} />
+    <Route path={"/verify-phonenumber:number"} component={VerifyPhone} />
+    <Route path={"/social-login"} component={SocialLogin} />
+    <Redirect from={"*"} to={"/"} />
+    {/* redirect는 마지막에 작성해야함 (존재하는 경로를 검사후 리다이렉트 실행) */}
+  </Switch>
+);
+
+const LoggedInRoutes: React.SFC = () => (
+  <Switch>
+    <Route path={"/"} exact={true} component={Home} />
+    <Route path={"/ride"} exact={true} component={Ride} />
+    <Route path={"/add-place"} exact={true} component={AddPlace} />
+    <Route path={"/edit-account"} exact={true} component={EditAccount} />
+    <Route path={"/places"} exact={true} component={Places} />
+    <Route path={"/settings"} exact={true} component={Settings} />
+    <Route path={"/find-address"} exact={true} component={FindAddress} />
+    <Redirect from={"*"} to={"/"} />
+  </Switch>
+);
 
 AppPresenter.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired
