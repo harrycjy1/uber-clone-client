3c69987ce33aff40750c2e4b6bda26c5635cacb3
choddol <harrycjy123@gmail.com>
2019-06-12 16:54:26 +0900

    hooks...?

--- src/Routes/EditAccount/EditAccountContainer.tsx
+++ src/Routes/EditAccount/EditAccountContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Mutation, Query } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -10,12 +10,14 @@ import {
 } from "../../types/api";
 import EditAccountPresenter from "./EditAccountPresenter";
 import { UPDATE_PROFILE } from "./EditAccountQueries";
+import { useQuery } from "react-apollo-hooks";
 
 interface IState {
   firstName: string;
   lastName: string;
   email: string;
   profilePhoto: string;
+  didFetch: boolean;
 }
 
 interface IProps extends RouteComponentProps<any> {}
@@ -25,56 +27,74 @@ class UpdateProfileMutation extends Mutation<
   updateProfileVariables
 > {}
 
-class ProfileQuery extends Query<userProfile> {}
+// class ProfileQuery extends Query<userProfile> {}
 
 class EditAccountContainer extends React.Component<IProps, IState> {
   public state = {
     email: "",
     firstName: "",
     lastName: "",
-    profilePhoto: ""
+    profilePhoto: "",
+    didFetch: false
+  };
+
+  ProfileQuery = () => {
+    const { email, firstName, lastName, profilePhoto, didFetch } = this.state;
+    const { data, loading } = useQuery(USER_PROFILE, {
+      fetchPolicy: "cache-and-network",
+      skip: didFetch
+    });
+
+    useEffect(() => {
+      const onCompleted = data => this.updateFields(data);
+      if (onCompleted) {
+        console.log(this.state);
+        onCompleted(data);
+      }
+    }, [data, loading]);
+
+    return (
+      <UpdateProfileMutation
+        mutation={UPDATE_PROFILE}
+        refetchQueries={[{ query: USER_PROFILE }]}
+        onCompleted={data => {
+          const { UpdateMyProfile } = data;
+          if (UpdateMyProfile.ok) {
+            toast.success("Profile updated!");
+          } else if (UpdateMyProfile.error) {
+            toast.error(UpdateMyProfile.error);
+          }
+        }}
+        variables={{
+          email,
+          firstName,
+          lastName,
+          profilePhoto
+        }}
+      >
+        {(updateProfileFn, { loading }) => (
+          <EditAccountPresenter
+            email={email}
+            firstName={firstName}
+            lastName={lastName}
+            profilePhoto={profilePhoto}
+            onInputChange={this.onInputChange}
+            loading={loading}
+            onSubmit={updateProfileFn}
+          />
+        )}
+      </UpdateProfileMutation>
+    );
   };
   public render() {
-    const { email, firstName, lastName, profilePhoto } = this.state;
     return (
-      <ProfileQuery
-        query={USER_PROFILE}
-        fetchPolicy={"cache-and-network"}
-        onCompleted={this.updateFields}
-      >
-        {() => (
-          <UpdateProfileMutation
-            mutation={UPDATE_PROFILE}
-            refetchQueries={[{ query: USER_PROFILE }]}
-            onCompleted={data => {
-              const { UpdateMyProfile } = data;
-              if (UpdateMyProfile.ok) {
-                toast.success("Profile updated!");
-              } else if (UpdateMyProfile.error) {
-                toast.error(UpdateMyProfile.error);
-              }
-            }}
-            variables={{
-              email,
-              firstName,
-              lastName,
-              profilePhoto
-            }}
-          >
-            {(updateProfileFn, { loading }) => (
-              <EditAccountPresenter
-                email={email}
-                firstName={firstName}
-                lastName={lastName}
-                profilePhoto={profilePhoto}
-                onInputChange={this.onInputChange}
-                loading={loading}
-                onSubmit={updateProfileFn}
-              />
-            )}
-          </UpdateProfileMutation>
-        )}
-      </ProfileQuery>
+      // <ProfileQuery
+      //   query={USER_PROFILE}
+      //   fetchPolicy={"cache-and-network"}
+      //   onCompleted={this.updateFields}
+      // >
+      <this.ProfileQuery />
+      // </ProfileQuery>
     );
   }
   public onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
@@ -88,19 +108,21 @@ class EditAccountContainer extends React.Component<IProps, IState> {
   };
 
   public updateFields = (data: userProfile) => {
-    if (data.GetMyProfile.user) {
-      const {
-        GetMyProfile: {
-          user: { firstName, lastName, email, profilePhoto }
+    if (data) {
+      const { GetMyProfile } = data;
+      if (GetMyProfile) {
+        const { user } = GetMyProfile;
+        if (user) {
+          const { firstName, lastName, email, profilePhoto } = user;
+          this.setState({
+            firstName,
+            lastName,
+            email,
+            profilePhoto,
+            didFetch: true
+          } as any);
         }
-      } = data;
-
-      this.setState({
-        firstName,
-        lastName,
-        email,
-        profilePhoto
-      } as any);
+      }
     }
   };
 }
