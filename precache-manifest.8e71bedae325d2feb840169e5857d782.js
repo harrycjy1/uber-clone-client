2f63c3a459204a435f076f6a20a001593f74d19b
Choddol <harrycjy123@gmail.com>
2019-07-25 22:12:49 +0900

    Updates

---
+++ precache-manifest.8e71bedae325d2feb840169e5857d782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a5c6b08249d38363d7896f972d05b1",
+    "url": "/uber-clone-client/index.html"
+  },
+  {
+    "revision": "1767a62e8aed79afda61",
+    "url": "/uber-clone-client/static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "1767a62e8aed79afda61",
+    "url": "/uber-clone-client/static/js/2.d1f4dfe8.chunk.js"
+  },
+  {
+    "revision": "1cbcb4a64ca6add908e9",
+    "url": "/uber-clone-client/static/js/main.187a9259.chunk.js"
+  },
+  {
+    "revision": "9b1668a328c3dfbeed78",
+    "url": "/uber-clone-client/static/js/runtime~main.37462add.js"
+  },
+  {
+    "revision": "f1662d11088621fd43b5f95d795d749e",
+    "url": "/uber-clone-client/static/media/bg.f1662d11.png"
+  }
+]);
