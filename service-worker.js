aeda57f770df4b1d1871bf8eb299cbe987bfb30d
Choddol <harrycjy123@gmail.com>
2019-07-25 23:41:56 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uber-clone-client/precache-manifest.9513b947f1c48610df5f9ba5cfb1547b.js"
+  "/uber-clone-client/precache-manifest.1c85d53910db414be2091c72f0f19bd5.js"
 );
 
 self.addEventListener('message', (event) => {
