1257e874a2f075e5f34f4bf676bc1fef25c4edbd
Choddol <harrycjy123@gmail.com>
2019-07-26 20:41:00 +0900

    still fixing

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,6 +1,10 @@
 import { GoogleApiWrapper } from "google-maps-react";
 import HomeContainer from "././HomeContainer";
 import { MAP_KEY } from "../../key";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const REAL_KEY = isDev ? MAP_KEY : process.env.MAP_KEY;
 export default GoogleApiWrapper({
-  apiKey: MAP_KEY
+  apiKey: REAL_KEY
 })(HomeContainer);
